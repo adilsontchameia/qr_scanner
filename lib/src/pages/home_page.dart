fd4f572a52dbc4e74082b25a278c769c71475381
Adilson Chameia <adilsonkchameia@outlook.com>
2022-01-20 20:17:53 +0100

    bottom naviagation

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -9,6 +9,24 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Text('Texto'),
       ),
+      bottomNavigationBar: _criarBottonNaviagation(),
+    );
+  }
+
+  Widget _criarBottonNaviagation() {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      onTap: (index) {},
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Primeiro'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_1),
+          title: Text('Primeiro'),
+        ),
+      ],
     );
   }
 }
